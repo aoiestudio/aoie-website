9c197361939a9ddf4dcd2ddaf2e91cf05cc62e06
Eduard Aksamitov <e@euaaaio.ru>
2026-05-14 01:24:43 +0300

    build: add autoprefixer to postcss pipeline

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 import postcssGlobalData from '@csstools/postcss-global-data'
+import autoprefixer from 'autoprefixer'
 import postcssCustomMedia from 'postcss-custom-media'
 
 export default {
   plugins: [
     postcssGlobalData({ files: ['src/styles/media.css'] }),
     postcssCustomMedia(),
+    autoprefixer(),
   ],
 }

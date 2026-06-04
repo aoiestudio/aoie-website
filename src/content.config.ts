d61e0054829644825eff95cd22a9ef43ac268fa0
Eduard Aksamitov <e@euaaaio.ru>
2026-06-04 23:27:17 +0300

    feat: add ogimage for projects

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,6 +14,7 @@ const projects = defineCollection({
     year: z.number().optional(),
     order: z.number(),
     images: z.array(z.string()),
+    ogImage: z.string().optional(),
     hero: z.array(z.string()).optional(),
     selected: z.array(z.string()).optional(),
   }),
